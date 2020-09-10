5f806931abd81445f5ffc498409d705035a564ef
Your Name <you@example.com>
2020-09-10 14:57:50 +0000

    fixign bug

--- src/polhemus_ros_driver/source_pose_calibration.py
+++ src/polhemus_ros_driver/source_pose_calibration.py
@@ -10,8 +10,8 @@ class SourcePoseCalibration(object):
         self.CONST_SOURCE_ORIENTATION = [0, 0.1, -pi]
         self.CONST_SOURCE_X_OFFSET = 0.008
         self.CONST_SOURCE_Z_OFFSET = 0.016
-        self.knuckle_thickness_accepted_range = range(0.015, 0.045)
-        self.knuckle_to_source_accepted_range = range(0.01, 0.06)
+        self.knuckle_thickness_accepted_range = [0.015, 0.045]
+        self.knuckle_to_source_accepted_range = [0.01, 0.06]
 
     def calibrate(self, knuckle_thickness, knuckle_to_source):
         self.check_if_values_within_range(knuckle_thickness, knuckle_to_source)
@@ -25,7 +25,7 @@ class SourcePoseCalibration(object):
         return [knuckle_thickness, knuckle_to_source]
 
     def check_if_values_within_range(self, knuckle_thickness, knuckle_to_source):
-        if not knuckle_thickness in self.knuckle_thickness_accepted_range:
+        if not self.knuckle_thickness_accepted_range[0] <= knuckle_thickness <= self.knuckle_thickness_accepted_range[1]:
             raise ValueError("Knuckle thickness outside of accepted range")
-        if not knuckle_to_source in self.knuckle_to_source_accepted_range:
+        if not self.knuckle_to_source_accepted_range[0] <= knuckle_to_source <= self.knuckle_to_source_accepted_range[1]:
             raise ValueError("Knuckle to source value outside of accepted range")
