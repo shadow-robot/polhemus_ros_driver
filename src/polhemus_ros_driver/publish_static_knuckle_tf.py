45822298711f096b780a7debbcdfe3b76b473fc2
maxzieba <max@shadowrobot.com>
2022-08-24 07:57:12 +0000

    fixing_gui

--- src/polhemus_ros_driver/publish_static_knuckle_tf.py
+++ src/polhemus_ros_driver/publish_static_knuckle_tf.py
@@ -33,7 +33,6 @@ class StaticKnuckleBroadcaster:
                 if key in line and "parent" in line:
                     connected_glove_sides.append(value)
                     break
-        rospy.logwarn(connected_glove_sides)
         return connected_glove_sides
 
     def publish_tf(self, req):
