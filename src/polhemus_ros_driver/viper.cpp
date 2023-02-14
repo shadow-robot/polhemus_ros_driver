b665b9e0709966b0731f545b333ac5f454a089f1
georgiablanco <georgia@shadowrobot.com>
2023-02-14 16:49:24 +0000

    linter

--- src/polhemus_ros_driver/viper.cpp
+++ src/polhemus_ros_driver/viper.cpp
@@ -1,6 +1,6 @@
 /*
 
- Copyright (C) 2022-23 Shadow Robot Company Ltd <software@shadowrobot.com>
+ Copyright (C) 2022 Shadow Robot Company Ltd <software@shadowrobot.com>
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
