4521e1403e14723b2f54a2695f7d8248def8a0fb
georgiablanco <georgia@shadowrobot.com>
2023-04-12 08:11:25 +0000

    adding logic to retry receive_data_frame

--- src/polhemus_ros_driver/viper.cpp
+++ src/polhemus_ros_driver/viper.cpp
@@ -105,15 +105,24 @@ int Viper::receive_data_frame(viper_cmds_e cmd_type)
     if (retval == 0)
     {
       CFrameInfo frame_info(g_rxbuf, g_nrxcount);
-      if ((fi.cmd() == -1) || (fi.action() == -1))
+      if ((frame_info.cmd() == -1) || (frame_info.action() == -1))
       {
-        ROS_INFO("[POLHEMUS] Message reply is not valid, retrying...");
-        ROS_DEBUG("[POLHEMUS] Error in message reply...");
-        ROS_DEBUG("reply cmd: %d", frame_info.cmd());
-        ROS_DEBUG("reply action: %d", frame_info.action());
-        ROS_DEBUG("cmd sent: %d", cmd_type);
-        retval = RETURN_ERROR;
-        continue;
+        if (attempt < attempts - 1)
+        {
+          ROS_WARN("[POLHEMUS] Message reply is not valid, retrying...");
+          ROS_DEBUG("[POLHEMUS] Attempt %d of %d", attempt + 1, attempts);
+          ROS_DEBUG("reply action: %d", frame_info.action());
+          ROS_DEBUG("cmd sent: %d", cmd_type);
+          continue;
+        }
+        else
+        {
+          ROS_ERROR("[POLHEMUS] Message reply is not valid after %d attempts, aborting...", attempts);
+          ROS_ERROR("reply action: %d", frame_info.action());
+          ROS_ERROR("cmd sent: %d", cmd_type);
+          retval = RETURN_ERROR;
+          break;
+        }
       }
       else if ((frame_info.cmd() != cmd_type) || !(frame_info.IsAck()))
       {
@@ -126,11 +135,10 @@ int Viper::receive_data_frame(viper_cmds_e cmd_type)
       }
       else
       {
-        ROS_DEBUG("Got expected message reply");
+        ROS_DEBUG("[POLHEMUS] Got expected message reply at attempt %d", attempt);
         break;
       }
     }
-    ros::Duration(0.5).sleep();
   }
   return retval;
 }
