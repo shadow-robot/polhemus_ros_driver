c4fd4dfede7070794d0f16654294ae2dd4482756
Jesús Ferrándiz <62122095+jeferal@users.noreply.github.com>
2023-06-27 10:59:54 +0100

    Update src/polhemus_ros_driver/viper.cpp
    Co-authored-by: Brice Denoun <44160426+bdenoun@users.noreply.github.com>

--- src/polhemus_ros_driver/viper.cpp
+++ src/polhemus_ros_driver/viper.cpp
@@ -419,7 +419,7 @@ bool Viper::persist_commands(void)
 int Viper::send_saved_calibration()
 {
   int return_value = RETURN_ERROR;
-  retval = set_device_to_receive_saved_calibration();
+  return_value = set_device_to_receive_saved_calibration();
   if (RETURN_ERROR == retval)
     return -1;
 
